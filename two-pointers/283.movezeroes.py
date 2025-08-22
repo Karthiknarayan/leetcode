eab914f4230f390059efc5f278a5e03b557318cd
karthik <karthiknarayankkn@gmail.com>
2025-08-22 15:51:38 +0530

    moved zeroes to end by keeping i as fixed until j is non zero

---
+++ two-pointers/283.movezeroes.py
@@ -0,0 +1,37 @@
+
+# Code
+# Testcase
+# Test Result
+# Test Result
+# 283. Move Zeroes
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+
+nums = [0,1,0,3,12]
+
+i,j=0,0
+
+for j in range(len(nums)):
+    if nums[j]!=0:
+        nums[i],nums[j]=nums[j],nums[i]
+        i+=1
+
+print(nums)
