30326473ab99a5b27855891ec6754b6608e0367d
karthik <karthiknarayankkn@gmail.com>
2025-09-03 17:38:34 +0530

    added container-with-most-water using 2 pointer

---
+++ two-pointers/11.container-with-most-water.py
@@ -0,0 +1,17 @@
+def maxarea(height):
+        left=0
+        right=len(height)-1
+        maxarea=0
+        while(left<right):
+            curarea=min(height[left],height[right])*(right-left)
+            maxarea=max(curarea,maxarea)
+            if (height[left]<height[right]):
+                left+=1
+            else:
+                right-=1
+
+        return maxarea 
+
+
+height=[1,8,6,2,5,4,8,3,7]
+print(maxarea(height))
