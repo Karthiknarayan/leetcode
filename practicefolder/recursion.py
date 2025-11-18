33b3dcde5fc2a89a0aeba0e33e679184ca5d8d98
karthik <karthiknarayankkn@gmail.com>
2025-11-18 16:10:57 +0530

    print 1 to N using recursion

--- practicefolder/recursion.py
+++ practicefolder/recursion.py
@@ -1,15 +1,12 @@
-class Recursion:
-    def __init__(self):
-        pass
+# print from 1 to n
 
-    def recurfunction(self, val):
-        print(val)
-        # here it is necessary to keep as less number as possible like <999
-        if (val<100):
-            self.recurfunction(val+1)
-        else:
-            exit()
-    
-class A:
-    r = Recursion()
-    r.recurfunction(1)
+class Recursion:
+    def f_of_n(i,n):
+        # base case
+        if i>n:
+            return
+        
+        print(i)
+        Recursion.f_of_n(i+1,n)
+
+Recursion.f_of_n(1,100)
